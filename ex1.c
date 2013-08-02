106aa9ae2a6617a357b6985e0db1bee7898e73c9
yuguo <qiaoyuguo2012@gmail.com>
2013-08-02 17:10:54 +0800

    finished ex1 for lab3

---
+++ ex1.c
@@ -0,0 +1,27 @@
+//
+// Simple inline assembly example
+// 
+// For JOS lab 1 exercise 1
+//
+
+#include <stdio.h>
+
+int
+main(int argc, char **argv)
+{
+  int x = 1;
+  printf("Hello x = %d\n", x);
+
+  asm("addl $1,%0"
+     : "=r" (x)
+     : "0" (x));
+
+  printf("Hello x = %d after increment\n", x);
+
+  if(x == 2){
+    printf("OK\n");
+  }
+  else{
+    printf("ERROR\n");
+  }
+}
